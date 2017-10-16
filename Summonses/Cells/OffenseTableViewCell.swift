4a8e02cde559cd38a27cd2f34a456d34f96eaef7
Pavel <igorshavlovsky@Luiss-MacBook-Pro.local>
2017-10-16 14:46:17 +0300

    commit with offenses

---
+++ Summonses/Cells/OffenseTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  OffenseTableViewCell.swift
+//  Summonses
+//
+//  Created by Igor Shavlovsky on 10/16/17.
+//  Copyright © 2017 neoviso. All rights reserved.
+//
+
+import UIKit
+
+class OffenseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Summonses/ViewControllers/SearchOffenceViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SearchOffenceViewController.swift
+//  Summonses
+//
+//  Created by Igor Shavlovsky on 10/16/17.
+//  Copyright © 2017 neoviso. All rights reserved.
+//
+
+import UIKit
+
+class SearchOffenceViewController: UIViewController {
+    var  offenses: [OffenseModel] = []
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "SEARCH WINDOW"
+        offenses =  Array(DataBaseManager.shared.realm.objects(OffenseModel.self))
+        tableView.register(UINib(nibName: "OffenseTableViewCell", bundle: nil), forCellReuseIdentifier: "offenseidentifierCell")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SearchOffenceViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return offenses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "offenseidentifierCell") as! OffenseTableViewCell
+        cell.title.text = offenses[indexPath.row].tittle
+        cell.number.text = offenses[indexPath.row].number
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 62
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+        
+    }
+
+    
+    
+
+}
