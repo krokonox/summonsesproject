d30748cb9eaa89138f74b97eb62bdb22e82e49ab
Pavel Budankov <pavel.budankov@gmail.com>
2017-10-16 19:20:15 +0300

    blue

--- Summonses/AppDelegate.swift
+++ Summonses/AppDelegate.swift
@@ -20,7 +20,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         DataBaseManager.shared.setupDatabase()
         DataBaseManager.shared.setupOffenseIfNeeds()
         // Override point for customization after application launch.
+        setupAppearance()
         return true
+    }
+    
+    func setupAppearance() {
+//        UINavigationBar.appearance().bar = UIColor.blue
+//        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().barTintColor = .customBlue
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        UINavigationBar.appearance().isTranslucent = false
+        UIApplication.shared.statusBarStyle = .lightContent
+        UITabBar.appearance().barTintColor = UIColor.customBlue
+        UITabBar.appearance().tintColor = UIColor.customRed
+//        UITabBarItem.appearance().
+        UITabBar.appearance().isTranslucent = false
+      //  UITabBarItem.appearance().image.
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.white], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.customRed], for: .selected)
+
+       // UITabBar.appearance().colo
+        //UITabBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Summonses/ViewControllers/MenuOffenceViewController.swift
+++ Summonses/ViewControllers/MenuOffenceViewController.swift
@@ -12,7 +12,7 @@ class MenuOffenceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.title = "SUMMONSES"
         // Do any additional setup after loading the view.
     }
 
