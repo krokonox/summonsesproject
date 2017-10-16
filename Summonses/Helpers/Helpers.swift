c70f06f0695e75bdf23d4ddf6ca135155e675e31
Pavel <igorshavlovsky@Luiss-MacBook-Pro.local>
2017-10-16 15:49:29 +0300

    Add corner button

---
+++ Summonses/Helpers/Helpers.swift
@@ -0,0 +1,27 @@
+//
+//  Helpers.swift
+//  Summonses
+//
+//  Created by Igor Shavlovsky on 10/16/17.
+//  Copyright © 2017 neoviso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIButton {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+
+    
+    
+}
